ad18ad6724b56828b4d8e283bc60e0fd56524774
piyushpatil614 <piyushpatil6140@gmail.com>
2023-01-12 18:43:09 +0530

    Done with all

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -3,6 +3,12 @@ import Quiz from './Quiz'
 
 
 export default function Blogs() {
+  const onmouseover=(e) => {
+     e.currentTarget.style.transform="scale(1.1)"
+   }
+   const onmouseout=(e) => {
+     e.currentTarget.style.transform="scale(1)"
+    }
   return (
     <>
     <div>
@@ -21,44 +27,44 @@ export default function Blogs() {
       </div>
        
       <div className="container">
-        <div className="row my-4">
-          <div className="col-4 shadow">
+        <div className="row my-4 ">
+          <div className="col-4 shadow" onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe className='rounded' width="300" height="200" src="https://www.youtube.com/embed/H7HdGVlT97Y?list=PLiY4RXBbgyw-WPCs9xxbEucHQJYibmkwy" title="Make tokens using ERC20 in Ethereum" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <h6>Make token using ERC20 in Ethereum</h6>
           </div>
-          <div className="col-4 shadow">
+          <div className="col-4 shadow" onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe className='rounded' width="300" height="200" src="https://www.youtube.com/embed/GM3ZnmM5rlw?list=PLiY4RXBbgyw-WPCs9xxbEucHQJYibmkwy" title="Mint token in ERC20 contract by paying ether" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <h6>Mint token in ERC20 contract by paying ether</h6>
           </div>
-          <div className="col-4 shadow">
+          <div className="col-4 shadow" onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe width="300" height="200" src="https://www.youtube.com/embed/GE6IRTLkvFg" title="ChatApp using Blockchain in 3 minutes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <h6>ChatApp using Blockchain in 3 minutes</h6>
           </div>
         </div>
         <div className="row my-4">
-          <div className="col-4 shadow">
+          <div className="col-4 shadow" onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe className='rounded' width="300" height="200" src="https://www.youtube.com/embed/ArXnfKMXI-g?list=PLiY4RXBbgyw-WPCs9xxbEucHQJYibmkwy" title="How to use AccessControl for different roles" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <h6>How to do use AccessControl for different roles</h6>
           </div>
-          <div className="col-4 shadow">
+          <div className="col-4 shadow" onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe className='rounded' width="300" height="200" src="https://www.youtube.com/embed/WvTGSYedSPE?list=PLiY4RXBbgyw-WPCs9xxbEucHQJYibmkwy" title="How to do unit testing of smart contracts in solidity using remix" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <h6>How to do unit testing of smart contracts in solidity using remix</h6>
           </div>
-          <div className="col-4 shadow">
+          <div className="col-4 shadow" onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe className='rounded' width="300" height="200" src="https://www.youtube.com/embed/hD5S6UjEDW8?list=PLiY4RXBbgyw-WPCs9xxbEucHQJYibmkwy" title="Part 1: How to create non-fungible tokens using ERC721?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <h6>How to create non-fungible tokens using ERC721</h6>
           </div>
         </div>
         <div className="row my-4 ">
-          <div className="col-4 shadow">
+          <div className="col-4 shadow" onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe className='rounded' width="300" height="200" src="https://www.youtube.com/embed/sQbJpDIDY2E?list=PLiY4RXBbgyw-WPCs9xxbEucHQJYibmkwy" title="Part 2: ERC 721 token URI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <h6>ERC 721 token URI</h6>
           </div>
-          <div className="col-4 shadow">
+          <div className="col-4 shadow" onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe className='rounded' width="300" height="200" src="https://www.youtube.com/embed/t3PXjxxaE6o" title="The term 'geth' is not recognized as the name of a cmdlet" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <h6>GETH NOT FOUND</h6>
           </div>
-          <div className="col-4 shadow">
+          <div className="col-4 shadow"onMouseOver={onmouseover} onMouseOut={onmouseout}>
           <iframe className='rounded' width="300" height="200" src="https://www.youtube.com/embed/_nbn8X031-s?list=PLiY4RXBbgyw-WPCs9xxbEucHQJYibmkwy" title="Pause function in contract by using openzeppelin" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <h6>Pause function in contract by using openzeppelin</h6>
           </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export default function Home() {
+
+  const onmouseover=(e) => {
+    e.currentTarget.style.transform="scale(1.1)"
+  }
+  const onmouseout=(e) => {
+     e.currentTarget.style.transform="scale(1)"
+   }
   return (
     <>
       <div className='text-center'>
@@ -18,8 +25,8 @@ export default function Home() {
           </div>
 
           <div className="row row-cols-1 row-cols-md-2 g-4  my-2 ">
-            <div className="col shadow">
-              <div className="card">
+            <div className="col" onMouseOver={onmouseover} onMouseOut={onmouseout}>
+              <div className="card shadow">
                 <img width={200} height={250} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs6svVG0vNnM1qMy2JD57YQ0nFGQRuMdCEKg&usqp=CAU" className="card-img-top border-bottom border-2" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">Spring Boot</h5>
@@ -28,7 +35,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="col ">
+            <div className="col" onMouseOver={onmouseover} onMouseOut={onmouseout}>
               <div className="card shadow">
                 <img width={200} height={250} src="https://i.ibb.co/bBbh9LJ/node.png" className="card-img-top border-bottom border-2" alt="..." />
                 <div className="card-body">
@@ -39,7 +46,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="col">
+            <div className="col" onMouseOver={onmouseover} onMouseOut={onmouseout}>
               <div className="card shadow">
                 <img width={200} height={250} src="https://i.ibb.co/jJLrrcL/flask.jpg" className="card-img-top border-bottom border-2" alt="..." />
                 <div className="card-body">
@@ -50,7 +57,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="col">
+            <div className="col" onMouseOver={onmouseover} onMouseOut={onmouseout}>
               <div className="card shadow">
                 <img width={200} height={250} src="https://i.ibb.co/HThg9Tc/React.jpg" className="card-img-top border-bottom border-2" alt="..." />
                 <div className="card-body">
@@ -60,7 +67,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="col">
+            <div className="col" onMouseOver={onmouseover} onMouseOut={onmouseout}>
               <div className="card shadow">
                 <img width={200} height={250} src="https://i.ibb.co/rpSY46P/solidity.png" className="card-img-top border-bottom border-2" alt="..." />
                 <div className="card-body">
@@ -71,7 +78,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="col">
+            <div className="col" onMouseOver={onmouseover} onMouseOut={onmouseout}>
               <div className="card shadow">
                 <img width={200} height={250} src="https://i.ibb.co/3WSV2xY/web3.webp" className="card-img-top border-bottom border-2" alt="..." />
                 <div className="card-body">
@@ -102,23 +109,23 @@ export default function Home() {
           <div className="container shadow">
             <div className="row my-3">
               <div className="col-3">
-                <img width={100} height={100} src="https://i.ibb.co/my8ppZr/mongodb.jpg" className="card-img-top border-bottom border-2" alt="..."/>
+                <img width={100} height={100} src="https://i.ibb.co/my8ppZr/mongodb.jpg" className="card-img-top border-bottom border-2" alt="..." onMouseOver={onmouseover} onMouseOut={onmouseout}/>
               </div>
               <div className="col-3">
-                <img width={100} height={100} src="https://i.ibb.co/yycZncZ/mysql.jpg" className="card-img-top border-bottom border-2" alt="..." />
+                <img width={100} height={100} src="https://i.ibb.co/yycZncZ/mysql.jpg" className="card-img-top border-bottom border-2" alt="..." onMouseOver={onmouseover} onMouseOut={onmouseout}/>
               </div>
               <div className="col-3">
-                <img width={100} height={100} src="https://i.ibb.co/nz3b1G0/python.png" className="card-img-top border-bottom border-2" alt="..." />
+                <img width={100} height={100} src="https://i.ibb.co/nz3b1G0/python.png" className="card-img-top border-bottom border-2" alt="..." onMouseOver={onmouseover} onMouseOut={onmouseout}/>
               </div>
               <div className="col-3">
-                <img width={100} height={100} src="https://i.ibb.co/0JWGRMd/java.jpg" className="card-img-top border-bottom border-2" alt="..." />
+                <img width={100} height={100} src="https://i.ibb.co/0JWGRMd/java.jpg" className="card-img-top border-bottom border-2" alt="..." onMouseOver={onmouseover} onMouseOut={onmouseout}/>
               </div>
               <div className="row my-3 justify-content-center">
                 <div className="col-3 align-self-center">
-                  <img width={100} height={100} src="https://i.ibb.co/n07d4j3/firebase.png" className="card-img-top border-bottom border-2" alt="..." />
+                  <img width={100} height={100} src="https://i.ibb.co/n07d4j3/firebase.png" className="card-img-top border-bottom border-2" alt="..." onMouseOver={onmouseover} onMouseOut={onmouseout}/>
                 </div>
                 <div className="col-3 align-self-center">
-                  <img width={100} height={100} src="https://i.ibb.co/tJp3SNC/html5.png" className="card-img-top border-bottom border-2" alt="..." />
+                  <img width={100} height={100} src="https://i.ibb.co/tJp3SNC/html5.png" className="card-img-top border-bottom border-2" alt="..." onMouseOver={onmouseover} onMouseOut={onmouseout}/>
                 </div>
               </div>
               
